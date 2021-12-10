86e8261d5eddc93123f8aea1509378a2a6b82a40
Phillip Douglas <phillip.duane.douglas.jr@gmail.com>
2021-12-10 17:24:17 -0500

    updates

--- finance-tracker/src/pages/signup/Signup.js
+++ finance-tracker/src/pages/signup/Signup.js
@@ -1,7 +1,45 @@
+// React components
+import { useState } from 'react';
+
+// styles
 import styles from './Signup.module.css';
 
 export default function Signup() {
+  // states
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // functions
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password);
+  }
+
   return (
-    <div>Signup</div>
+    <form className="signup-form" action="" >
+      <label>
+        <span>dispaly name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+    </form>
   )
 }
