85840dc1b1d724780f1f9a5d2f4034a4839791aa
Phillip Douglas <phillip.duane.douglas.jr@gmail.com>
2021-12-10 11:40:17 -0500

    finished

--- finance-tracker/src/components/navigation/Navbar.js
+++ finance-tracker/src/components/navigation/Navbar.js
@@ -1,9 +1,23 @@
+// Routing components
+import { Link } from 'react-router-dom';
+
+// styles
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      Navbar
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>
+          <Link to="/">Finance Tracker</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
