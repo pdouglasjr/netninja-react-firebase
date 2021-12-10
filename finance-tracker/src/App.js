88f43801ffa021c5a40c7cd89dc94fb31385c0fd
Phillip Douglas <phillip.duane.douglas.jr@gmail.com>
2021-12-10 01:02:53 -0500

    updates

---
+++ finance-tracker/src/App.js
@@ -0,0 +1,29 @@
+// routing components
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// pages
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
