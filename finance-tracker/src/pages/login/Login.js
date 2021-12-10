7d5a3c728fa1f84de010ab543c2a740f3e3d72fe
Phillip Douglas <phillip.duane.douglas.jr@gmail.com>
2021-12-10 16:47:59 -0500

    updates

--- finance-tracker/src/pages/login/Login.js
+++ finance-tracker/src/pages/login/Login.js
@@ -1,9 +1,40 @@
+// React components
+import { useState } from 'react';
+
+// styles
 import styles from './Login.module.css';
 
 export default function Login() {
+  // state
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form className={styles['login-form']} action="" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn" type="submit">Log In</button>
+    </form>
   )
 }
